c1fef0a62b03872858728d16e194329c2abcfcc9
Rafael Verona <raverona@gmail.com>
2020-04-22 16:10:32 -0300

    Delete default test file

--- test/clojure_for_the_brave_and_true/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-for-the-brave-and-true.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-for-the-brave-and-true.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
