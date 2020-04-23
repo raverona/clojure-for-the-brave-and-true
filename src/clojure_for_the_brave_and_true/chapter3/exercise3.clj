04aeae40955a3966a1ad997a4eac18ab5bf1dd05
Rafael Verona <raverona@gmail.com>
2020-04-23 11:59:12 -0300

    Add chapter 3: exercise 3

--- src/clojure_for_the_brave_and_true/chapter3/exercise3.clj
+++ src/clojure_for_the_brave_and_true/chapter3/exercise3.clj
@@ -1,1 +1,6 @@
 (ns clojure-for-the-brave-and-true.chapter3.exercise3)
+
+(defn dec-maker
+  "Returns a function that subtracts a number by the factor supplied"
+  [decrease-factor]
+  #(- % decrease-factor))
