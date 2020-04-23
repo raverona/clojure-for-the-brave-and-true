3c5df5808c094873335db7a54b2cde80760fa5bb
Rafael Verona <raverona@gmail.com>
2020-04-23 14:51:47 -0300

    Add file for bonus exercise 1

---
+++ src/clojure_for_the_brave_and_true/chapter3/bonus1.clj
@@ -0,0 +1,3 @@
+(ns clojure-for-the-brave-and-true.chapter3.bonus1)
+
+; TODO: implement `expand-body-parts`, a function that works like `symmetrize-body-parts`, but takes an expander function that tells you how to expand the body parts for different creatures
