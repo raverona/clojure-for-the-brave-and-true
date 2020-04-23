e8b92bd885881a98c1fd531926c29b30fa48b28f
Rafael Verona <raverona@gmail.com>
2020-04-23 11:59:28 -0300

    Add chapter 3: exercise 3 tests

--- test/clojure_for_the_brave_and_true/chapter3/exercise3_test.clj
+++ test/clojure_for_the_brave_and_true/chapter3/exercise3_test.clj
@@ -1,1 +1,13 @@
-(ns clojure-for-the-brave-and-true.chapter3.exercise3-test)
+(ns clojure-for-the-brave-and-true.chapter3.exercise3-test
+  (:require [clojure-for-the-brave-and-true.chapter3.exercise3 :as chapter3.exercise3]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]))
+
+(defspec testing-dec-maker
+         100
+         (prop/for-all [number gen/large-integer
+                        decrease-factor gen/large-integer]
+                       (let [dec-fn (chapter3.exercise3/dec-maker decrease-factor)]
+                         (= (- number decrease-factor)
+                            (dec-fn number)))))
