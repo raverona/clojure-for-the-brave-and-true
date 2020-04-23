86a3a424acb85dcf204c44dc0b714cd046591465
Rafael Verona <raverona@gmail.com>
2020-04-23 09:50:49 -0300

    Add test.check library, for property testing

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "1.0.0"]]}}
   :repl-options {:init-ns clojure-for-the-brave-and-true.core})
