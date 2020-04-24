4d0aa7f54ff997bb433d5761429fb0f47c347761
Rafael Verona <raverona@gmail.com>
2020-04-24 11:48:00 -0300

    Add chapter 3: exercise 4 tests

--- test/clojure_for_the_brave_and_true/chapter3/exercise4_test.clj
+++ test/clojure_for_the_brave_and_true/chapter3/exercise4_test.clj
@@ -1,1 +1,17 @@
-(ns clojure-for-the-brave-and-true.chapter3.exercise4-test)
+(ns clojure-for-the-brave-and-true.chapter3.exercise4-test
+  (:require [clojure-for-the-brave-and-true.chapter3.exercise4 :as chapter3.exercise4]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]))
+
+; A random transformation function to test the mapset function
+(defn- mod-10
+  "Returns the remainder of the provided number divided by 10"
+  [x]
+  (rem x 10))
+
+(defspec testing-mapset
+         100
+         (prop/for-all [collection (gen/vector gen/large-integer)]
+                       (= (into #{} (map mod-10 collection))
+                          (chapter3.exercise4/mapset mod-10 collection))))
