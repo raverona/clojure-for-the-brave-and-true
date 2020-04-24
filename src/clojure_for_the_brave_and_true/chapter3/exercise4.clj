c679c1aaa43a5bf3b25d564ea99cf611ca0c5153
Rafael Verona <raverona@gmail.com>
2020-04-24 11:47:30 -0300

    Add chapter 3: exercise 4

--- src/clojure_for_the_brave_and_true/chapter3/exercise4.clj
+++ src/clojure_for_the_brave_and_true/chapter3/exercise4.clj
@@ -1,1 +1,14 @@
 (ns clojure-for-the-brave-and-true.chapter3.exercise4)
+
+(defn do-mapset
+  [f coll set]
+  (if (not-empty coll)
+    (recur f (rest coll) (conj set (f (first coll))))
+    set))
+
+(defn mapset
+  "The map function, but it returns a set.
+   Obs: At this point the book only mentions the usage of the map function
+        with a function and one collection, so that's the only arity implemented here."
+  [f coll]
+  (do-mapset f coll #{}))
