cf2359d1222605809730b61354e101d7c396cf99
Rafael Verona <raverona@gmail.com>
2020-05-06 17:32:48 -0300

    Add chapter 3: bonus exercise 1

--- src/clojure_for_the_brave_and_true/chapter3/bonus1.clj
+++ src/clojure_for_the_brave_and_true/chapter3/bonus1.clj
@@ -1,3 +1,14 @@
 (ns clojure-for-the-brave-and-true.chapter3.bonus1)
 
-; TODO: implement `expand-body-parts`, a function that works like `symmetrize-body-parts`, but takes an expander function that tells you how to expand the body parts for different creatures
+; TODO: implement `expand-body-parts`, a function that works like `symmetrize-body-parts`,
+; but takes an expander function that tells you how to expand the body parts for different creatures
+
+(defn expand-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts expander-fn]
+  (reduce (fn
+            [symmetric-body-parts body-part]
+            (into symmetric-body-parts
+                  (expander-fn body-part)))
+          []
+          asym-body-parts))
