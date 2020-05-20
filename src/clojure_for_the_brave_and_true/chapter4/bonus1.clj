fab98c9a56a08ba40d9e26f051e03c478a9f86f8
Rafael Verona <raverona@gmail.com>
2020-05-20 00:49:27 -0300

    Add chapter 4 bonus exercises 1 and 2

---
+++ src/clojure_for_the_brave_and_true/chapter4/bonus1.clj
@@ -0,0 +1,3 @@
+(ns clojure-for-the-brave-and-true.chapter4.bonus1)
+
+; TODO: implement `map` using `reduce`
---
+++ src/clojure_for_the_brave_and_true/chapter4/bonus2.clj
@@ -0,0 +1,3 @@
+(ns clojure-for-the-brave-and-true.chapter4.bonus2)
+
+; TODO: implement `filter` using `some`
