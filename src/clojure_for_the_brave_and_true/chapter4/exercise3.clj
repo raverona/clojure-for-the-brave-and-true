6e72188dc74195d25f80cd0356d016487dcdac56
Rafael Verona <raverona@gmail.com>
2020-05-28 20:56:30 -0300

    Add chapter 4: exercise 3

--- src/clojure_for_the_brave_and_true/chapter4/exercise3.clj
+++ src/clojure_for_the_brave_and_true/chapter4/exercise3.clj
@@ -1,1 +1,13 @@
 (ns clojure-for-the-brave-and-true.chapter4.exercise3)
+
+(defn validate
+  [validation-map record]
+  (loop [remaining-validations validation-map]
+    (if (empty? remaining-validations)
+      true
+      (let [[curr & rest] remaining-validations
+            validation-key  (get curr 0)
+            validation-fn   (get curr 1)]
+        (if (validation-fn (get record validation-key))
+          (recur (rest remaining-validations))
+          false)))))
--- test/clojure_for_the_brave_and_true/chapter4/exercise3_test.clj
+++ test/clojure_for_the_brave_and_true/chapter4/exercise3_test.clj
@@ -1,1 +1,29 @@
-(ns clojure-for-the-brave-and-true.chapter4.exercise3_test)
+(ns clojure-for-the-brave-and-true.chapter4.exercise3_test
+  (:require [clojure.test :refer :all]
+            [clojure-for-the-brave-and-true.chapter4.exercise3 :as chapter4.exercise3]))
+
+(defn validate-name
+  [name]
+  (and (= String (type name))
+       (not-empty name)))
+
+(defn validate-glitter-index
+  [glitter-index]
+  (and (= Integer (type glitter-index))
+       (> glitter-index 0)))
+
+(def validation-map {:name validate-name
+                     :glitter-index validate-glitter-index})
+
+(def valid-record {:name "Eric Brooks" :glitter-index 0})
+
+(def invalid-record {:name "Eric Brooks"})
+
+(deftest test-validate
+  (testing "validate returns true if record is valid"
+    (is (= true
+           (chapter4.exercise3/validate validation-map valid-record))))
+
+  (testing "validate returns false if record is valid"
+    (is (= false
+         (chapter4.exercise3/validate validation-map invalid-record)))))
