6ff3026bbff5982fc644617e51e0dd5a5117fbba
Rafael Verona <raverona@gmail.com>
2020-05-28 16:01:55 -0300

    Add chapter 4: exercise 2

--- src/clojure_for_the_brave_and_true/chapter4/exercise2.clj
+++ src/clojure_for_the_brave_and_true/chapter4/exercise2.clj
@@ -1,1 +1,8 @@
 (ns clojure-for-the-brave-and-true.chapter4.exercise2)
+
+(defn append-suspects
+  [old-suspects new-suspects]
+  (->> new-suspects
+       (map #(str "\n" (:name %) "," (:glitter-index %)))
+       (reduce #(str %1 %2))
+       (str old-suspects)))
--- test/clojure_for_the_brave_and_true/chapter4/exercise2_test.clj
+++ test/clojure_for_the_brave_and_true/chapter4/exercise2_test.clj
@@ -1,1 +1,16 @@
-(ns clojure-for-the-brave-and-true.chapter4.exercise2_test)
+(ns clojure-for-the-brave-and-true.chapter4.exercise2_test
+  (:require [clojure.test :refer :all]
+            [clojure-for-the-brave-and-true.chapter4.exercise2 :as chapter4.exercise2]))
+
+(def file-content "Edward Cullen,10\nBella Swan,0\nCharlie Swan,0\nJacob Black,3\nCarlisle Cullen,6")
+
+(def new-suspects '({:name "Deacon Frost" :glitter-index 5}
+                    {:name "Nyssa Damaskinos" :glitter-index 3}
+                    {:name "Abraham Whistler" :glitter-index 1}))
+
+(def file-content-with-new-suspects-appended "Edward Cullen,10\nBella Swan,0\nCharlie Swan,0\nJacob Black,3\nCarlisle Cullen,6\nDeacon Frost,5\nNyssa Damaskinos,3\nAbraham Whistler,1")
+
+(deftest test-append
+  (testing "append should write to the end of suspects list"
+    (is (= file-content-with-new-suspects-appended
+           (chapter4.exercise2/append-suspects file-content new-suspects)))))
