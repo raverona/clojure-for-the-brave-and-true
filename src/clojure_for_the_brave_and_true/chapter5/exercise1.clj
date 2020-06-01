a4571d958f5f981e4cdca517c906a299025b2b22
Rafael Verona <raverona@gmail.com>
2020-06-01 19:00:20 -0300

    Add chapter 5: exercise 1

--- src/clojure_for_the_brave_and_true/chapter5/exercise1.clj
+++ src/clojure_for_the_brave_and_true/chapter5/exercise1.clj
@@ -1,1 +1,11 @@
 (ns clojure-for-the-brave-and-true.chapter5.exercise1)
+
+(def ^:private character
+  {:name "Smooches McCutes"
+   :attributes {:intelligence 10
+                :strength 4
+                :dexterity 5}})
+
+(defn attr
+  [attr-name]
+  ((comp attr-name :attributes) character))
---
+++ test/clojure_for_the_brave_and_true/chapter5/exeercise1_test.clj
@@ -0,0 +1,17 @@
+(ns clojure-for-the-brave-and-true.chapter5.exeercise1-test
+  (:require [clojure.test :refer :all]
+            [clojure-for-the-brave-and-true.chapter5.exercise1 :as chapter5.exercise1]))
+
+
+(deftest test-attr
+  (testing "attr should return 10 for attribute intelligence"
+    (is (= 10
+           (chapter5.exercise1/attr :intelligence))))
+
+  (testing "attr should return 4 for attribute strength"
+    (is (= 4
+           (chapter5.exercise1/attr :strength))))
+
+  (testing "attr should return 5 for attribute dexterity"
+    (is (= 5
+           (chapter5.exercise1/attr :dexterity)))))
