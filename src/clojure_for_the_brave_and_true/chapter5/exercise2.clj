af29df64b4d2f6f031fe243237c9a81a73349fd5
Rafael Verona <raverona@gmail.com>
2020-06-02 14:49:06 -0300

    Add chapter 5: exercise 2

--- src/clojure_for_the_brave_and_true/chapter5/exercise2.clj
+++ src/clojure_for_the_brave_and_true/chapter5/exercise2.clj
@@ -1,1 +1,9 @@
 (ns clojure-for-the-brave-and-true.chapter5.exercise2)
+
+(defn my-comp
+  ([] identity)
+  ([f] f)
+  ([f & fs]
+   (fn
+     [& args]
+     (f (apply (apply my-comp fs) args)))))
---
+++ test/clojure_for_the_brave_and_true/chapter5/exercise2_test.clj
@@ -0,0 +1,34 @@
+(ns clojure-for-the-brave-and-true.chapter5.exercise2_test
+  (:require [clojure.test :refer :all]
+            [clojure-for-the-brave-and-true.chapter5.exercise2 :as chapter5.exercise2]))
+
+(def f1 #(* % 2))
+(def f2 inc)
+(def f3 #(/ % 2))
+(def f4 dec)
+(def f5 #(- % 2))
+
+(deftest test-my-comp
+  (testing "composition of nothing should be identity"
+    (is (= ((comp) 1)
+           ((chapter5.exercise2/my-comp) 1))))
+
+  (testing "composition of f1 should be f1 itself"
+    (is (= ((comp f1) 1)
+           ((chapter5.exercise2/my-comp f1) 1))))
+
+  (testing "composition of f1 and f2 should be f1(f2(x))"
+    (is (= ((comp f1 f2) 1)
+           ((chapter5.exercise2/my-comp f1 f2) 1))))
+
+  (testing "composition of f1, f2 and f3 should be f1(f2(f3(x)))"
+    (is (= ((comp f1 f2 f3) 4)
+           ((chapter5.exercise2/my-comp f1 f2 f3) 4))))
+
+  (testing "composition of f1, f2, f3 and f4 should be f1(f2(f3(f4(x))))"
+    (is (= ((comp f1 f2 f3 f4) 7)
+           ((chapter5.exercise2/my-comp f1 f2 f3 f4) 7))))
+
+  (testing "composition of f1, f2, f3, f4 and f5 should be f1(f2(f3(f4(f5(x)))))"
+    (is (= ((comp f1 f2 f3 f4 f5) 17)
+           ((chapter5.exercise2/my-comp f1 f2 f3 f4 f5) 17)))))
