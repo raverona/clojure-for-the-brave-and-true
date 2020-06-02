b1cb34e453316cb69ed331423847bd3949b6fe21
Rafael Verona <raverona@gmail.com>
2020-06-02 15:41:06 -0300

    Add chapter 5: exercise 3

--- src/clojure_for_the_brave_and_true/chapter5/exercise3.clj
+++ src/clojure_for_the_brave_and_true/chapter5/exercise3.clj
@@ -1,1 +1,7 @@
 (ns clojure-for-the-brave-and-true.chapter5.exercise3)
+
+(defn my-assoc-in
+  ([m [k & ks] v]
+   (if (nil? ks)
+     (assoc m k v)
+     (assoc m k (my-assoc-in {} ks v)))))
---
+++ test/clojure_for_the_brave_and_true/chapter5/exercise3_test.clj
@@ -0,0 +1,20 @@
+(ns clojure-for-the-brave-and-true.chapter5.exercise3_test
+  (:require [clojure.test :refer :all]
+            [clojure-for-the-brave-and-true.chapter5.exercise3 :as chapter5.exercise3]))
+
+(deftest test-my-assoc-in
+  (testing "associating one key to an empty map should create a key/value pair"
+    (is (= (assoc-in {} [:key] "value")
+           (chapter5.exercise3/my-assoc-in {} [:key] "value"))))
+
+  (testing "associating multiple keys to an empty map should create nested key/value pairs"
+    (is (= (assoc-in {} [:some-key :other-key :another-key] "value")
+           (chapter5.exercise3/my-assoc-in {} [:some-key :other-key :another-key] "value"))))
+
+  (testing "associating one key to a map with existing key/value pairs should create a new key/value pair and keep the old ones"
+    (is (= (assoc-in {:some-key "some-value"} [:other-key] "other-value")
+           (chapter5.exercise3/my-assoc-in {:some-key "some-value"} [:other-key] "other-value"))))
+
+  (testing "associating multiple keys to a map with existing key/value pairs should create nested key/value pairs and keep the old ones"
+    (is (= (assoc-in {:some-key "some-value"} [:other-key :another-key] "other-value")
+           (chapter5.exercise3/my-assoc-in {:some-key "some-value"} [:other-key :another-key] "other-value")))))
