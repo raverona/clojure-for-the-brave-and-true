c2fc7f2e7d22a7bf0f5cda33d8bc6779026923a8
Rafael Verona <raverona@gmail.com>
2020-07-08 19:34:19 -0300

    Add chapter 7 exercise 1

--- src/clojure_for_the_brave_and_true/chapter7/exercise1.clj
+++ src/clojure_for_the_brave_and_true/chapter7/exercise1.clj
@@ -1,1 +1,5 @@
 (ns clojure-for-the-brave-and-true.chapter7.exercise1)
+
+(read-string "(print \"I'm Rafael and I like Star Wars\")")
+(list 'print "I'm Rafael and I like Star Wars")
+'(print "I'm Rafael and I like Star Wars")
--- test/clojure_for_the_brave_and_true/chapter7/exercise1_test.clj
+++ test/clojure_for_the_brave_and_true/chapter7/exercise1_test.clj
@@ -1,2 +1,9 @@
 (ns clojure-for-the-brave-and-true.chapter7.exercise1-test
   (:require [clojure.test :refer :all]))
+
+
+(deftest test-clojure-reader
+  (testing "forms to be evaluated can be created with quoting and the read-string function"
+    (is (= (read-string "(print \"I'm Rafael and I like Star Wars\")")
+           (list 'print "I'm Rafael and I like Star Wars")
+           '(print "I'm Rafael and I like Star Wars")))))
